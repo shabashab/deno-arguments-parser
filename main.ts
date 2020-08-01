c48b6958f214e779fdaea3a26ba64f955cd91601
shabashab <shabashab.04@gmail.com>
2020-08-01 12:19:12 +0300

    Refactor

--- main.ts
+++ main.ts
@@ -1,35 +1,7 @@
-import ArgumentsParser from "./ArgumentsParser.ts";
-import ArgumentsPrototype from "./ArgumentsPrototype.ts";
-import Argument from "./Argument.ts";
+import Argument from "./src/Argument.ts";
+import ArgumentsPrototype from "./src/ArgumentsPrototype.ts";
 
-class Prototype extends ArgumentsPrototype {
-  @Argument(
-    "n",
-    "name",
-  )
-  name: string;
-
-  @Argument(
-    "s",
-    "sname",
-  )
-  sName: string[];
-
-  @Argument(
-    "e",
-  )
-  exists: boolean;
-
-  constructor() {
-    super();
-    this.name = "";
-    this.sName = [];
-    this.exists = false;
-  }
-}
-
-let proto = new Prototype();
-proto.parseArgs(Deno.args);
-
-console.log(proto.name);
-console.log(proto.sName);
+export {
+  Argument,
+  ArgumentsPrototype,
+};
