2a374325e496fb85c89f0c7da9f2e238f9814895
shabashab <shabashab.04@gmail.com>
2020-08-01 13:02:11 +0300

    Fix: ArgumentsParser can parse long names

--- src/ArgumentsParser.ts
+++ src/ArgumentsParser.ts
@@ -1,6 +1,33 @@
 import ArgumentsPrototype from "./ArgumentsPrototype.ts";
 
 export default class ArgumentsParser {
+  private static parseArgumentById(
+    index: number,
+    proto: any,
+    argv: string[],
+    key: string,
+  ) {
+    if (typeof proto[key] === typeof new Array<string>()) {
+      proto[key] = new Array<string>();
+      for (
+        let add = 1;
+        argv.length > index + add &&
+        !argv[index + add].startsWith("-");
+        add++
+      ) {
+        (proto[key] as string[]).push(argv[index + add]);
+      }
+    } else if (typeof proto[key] === typeof new String().valueOf()) {
+      if (argv.length > index + 1) {
+        if (!argv[index + 1].startsWith("-")) {
+          proto[key] = argv[index + 1];
+        }
+      }
+    } else if (typeof proto[key] === typeof new Boolean().valueOf()) {
+      proto[key] = true;
+    }
+  }
+
   public static parse<T extends ArgumentsPrototype>(
     argv: string[],
     prototype: T,
@@ -9,28 +36,16 @@ export default class ArgumentsParser {
 
     for (let key in prototype.argumentsBindings) {
       const shortName = prototype.argumentsBindings[key].shortName;
+      const longName = prototype.argumentsBindings[key].longName;
 
       if (argv.includes(`-${shortName}`)) {
         const index = argv.indexOf(`-${shortName}`);
-        if (typeof anyProto[key] === typeof new Array<string>()) {
-          anyProto[key] = new Array<string>();
-          for (
-            let add = 1;
-            argv.length > index + add &&
-            !argv[index + add].startsWith("-");
-            add++
-          ) {
-            (anyProto[key] as string[]).push(argv[index + add]);
-          }
-        } else if (typeof anyProto[key] === typeof new String().valueOf()) {
-          if (argv.length > index + 1) {
-            if (!argv[index + 1].startsWith("-")) {
-              anyProto[key] = argv[index + 1];
-            }
-          }
-        } else if (typeof anyProto[key] === typeof new Boolean().valueOf()) {
-          anyProto[key] = true;
-        }
+        this.parseArgumentById(index, anyProto, argv, key);
+      }
+
+      if (argv.includes(`--${longName}`)) {
+        const index = argv.indexOf(`--${longName}`);
+        this.parseArgumentById(index, anyProto, argv, key);
       }
     }
 
